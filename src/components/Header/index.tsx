2fe817584ec8145bbb9999f2d69ca4eb38477047
Jackie Trang <trangpn90@gmail.com>
2022-03-01 21:00:40 +0700

    fix error location

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useAuth from 'src/Hooks/useAuth';
 
 function Header() {
   const auth = useAuth();
+  const location = useLocation();
   const linkLogin = auth.isAuthenticated ? location.pathname : "/";
 
   return (
     <div>
-      {console.log('LinkLogin: ', location)}
       <ul>
         <li>
           <Link to={linkLogin} >Login</Link>
