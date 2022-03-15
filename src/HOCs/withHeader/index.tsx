289add675d620bb0219627d0cfbc433f731cf1d9
Jackie Trang <trangpn90@gmail.com>
2022-03-16 05:36:54 +0700

    work arround typescript with HOCs

--- src/HOCs/withHeader/index.tsx
+++ src/HOCs/withHeader/index.tsx
@@ -1,16 +1,19 @@
-import type React from 'react';
+import React from 'react';
 import './Header.scss';
 
-function withHeader(Component: any) {
-  return (
-    <div className="header bg-gradient-default pb-8 pt-5 pt-md-8">
-      <div className="container-fluid">
-        <div className="header-body">
-          <Component></Component>
+const withHeader = (Component: any) =>
+class WithHeader extends React.Component {
+  render() {
+    return (
+      <div className="header bg-gradient-default pb-8 pt-5 pt-md-8">
+        <div className="container-fluid">
+          <div className="header-body">
+            <Component></Component>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default withHeader;
