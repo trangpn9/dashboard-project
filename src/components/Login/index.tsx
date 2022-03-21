36f30103b61541aef14c5520cd9f7767636a8342
Jackie Trang <trangpn90@gmail.com>
2022-03-22 06:45:40 +0700

    edit login form

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -4,7 +4,7 @@ import * as yup from "yup";
 import "./Login.scss";
 import Logo from './../../assets/images/logo.png';
 import useAuth from './../../Hooks/useAuth';
-import {useNavigate, Navigate, useLocation} from 'react-router-dom';
+import { useNavigate, Navigate, useLocation } from 'react-router-dom';
 import { Col, Row, Container, Button, Form, InputGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -19,15 +19,15 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   let navigate = useNavigate();
-  let location:any = useLocation();
+  let location: any = useLocation();
   let auth = useAuth();
 
   let from = location.state?.from?.pathname || "/dashboard";
 
   useEffect(() => {
     console.log('Pathname: ', location.state?.from?.pathname);
-    if(auth.user) return navigate(from, { replace: true });
-  },[])
+    if (auth.user) return navigate(from, { replace: true });
+  }, [])
 
   const handleOnSubmit = (values: any) => {
     auth.signin(values.username, () => {
@@ -59,104 +59,114 @@ const Login = () => {
   };
 
   return auth.isAuthenticated ? <Navigate to={from} /> : (
-    <Container className="h-100" id="wrapLoginForm" fluid>
-      <Row className="justify-content-center loginForm">
-        <Col xs={10} sm={6} lg={4} xxl={3} className="wrapLoginForm p-5">
-          <div className="d-flex justify-content-center m-4">
-            <div className="brand_logo_container">
-              <ErrorBoundary>
-                <img src={Logo} className="brand_logo" alt="Logo Dashboard Project" />
-              </ErrorBoundary>
+    <div id="idWrapLoginForm" className="h-100">
+      <div className="header bg-gradient-info py-7 py-lg-8">
+        <div className="space-free-login">          
+        </div>
+        <div className="separator separator-bottom separator-skew zindex-100"><svg xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+          <polygon className="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg></div>
+      </div>
+      <Container fluid className="containerFormLogin">
+        <Row className="justify-content-center loginForm">
+          <Col xs={10} sm={6} lg={4} xxl={3} className="wrapLoginForm p-5">
+            <div className="d-flex justify-content-center m-4">
+              <div className="brand_logo_container">
+                <ErrorBoundary>
+                  <img src={Logo} className="brand_logo" alt="Logo Dashboard Project" />
+                </ErrorBoundary>
+              </div>
             </div>
-          </div>
-          <h1 className="textWelcome mt-5 mb-4 text-center">Welcome to Jackie CMS!</h1>
-          <Formik
-            validationSchema={schema}
-            onSubmit={handleOnSubmit}
-            initialValues={{
-              username: "",
-              password: "",
-              rememberMe: false,
-            }}
-          >
-            {({
-              handleSubmit,
-              handleBlur,
-              handleChange,
-              values,
-              touched,
-              isValid,
-              dirty,
-              errors
-            }) => (
-              <Form noValidate onSubmit={handleSubmit}>
-                <Form.Group className="mb-3 mx-4">
-                  <InputGroup hasValidation>
-                    <InputGroup.Text id="username">
-                      <FontAwesomeIcon icon={faUserAlt} />
-                    </InputGroup.Text>
-                    <Form.Control
-                      type="text"
-                      placeholder="Username"
-                      aria-describedby="unsername"
-                      name="username"
-                      value={values.username}
-                      onChange={handleChange}
-                      isInvalid={!!errors.username && touched.username}
-                      className="shadow-none"
-                      onBlur={handleBlur}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      {!!errors.username && touched.username ? errors.username : null}
-                    </Form.Control.Feedback>
-                  </InputGroup>
-                </Form.Group>
-                <Form.Group className="mb-3 mx-4">
-                  <InputGroup hasValidation>
-                    <InputGroup.Text id="password">
-                      <FontAwesomeIcon icon={faKey} />
-                    </InputGroup.Text>
-                    <Form.Control
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Password"
-                      aria-describedby="password"
-                      name="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      isInvalid={!!errors.password && touched.password}
-                      className="shadow-none inputPassword"
-                      onBlur={handleBlur}
-                    />
-                    <InputGroup.Text id="iconShowPassword" className={!!errors.password && touched.password ? "isInvalid" : "isValid"} onClick={handleShowPassword}>
-                      <FontAwesomeIcon
-                        icon={showPassword ? faEye : faEyeSlash}
+            <h1 className="textWelcome mt-5 mb-4 text-center">Welcome to Jackie CMS!</h1>
+            <Formik
+              validationSchema={schema}
+              onSubmit={handleOnSubmit}
+              initialValues={{
+                username: "",
+                password: "",
+                rememberMe: false,
+              }}
+            >
+              {({
+                handleSubmit,
+                handleBlur,
+                handleChange,
+                values,
+                touched,
+                isValid,
+                dirty,
+                errors
+              }) => (
+                <Form noValidate onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3 mx-4">
+                    <InputGroup hasValidation>
+                      <InputGroup.Text id="username">
+                        <FontAwesomeIcon icon={faUserAlt} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        type="text"
+                        placeholder="Username"
+                        aria-describedby="unsername"
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        isInvalid={!!errors.username && touched.username}
+                        className="shadow-none"
+                        onBlur={handleBlur}
                       />
-                    </InputGroup.Text>
-                    <Form.Control.Feedback type="invalid">              
-                      {!!errors.password && touched.password ? errors.password : null}
+                      <Form.Control.Feedback type="invalid">
+                        {!!errors.username && touched.username ? errors.username : null}
+                      </Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group className="mb-3 mx-4">
+                    <InputGroup hasValidation>
+                      <InputGroup.Text id="password">
+                        <FontAwesomeIcon icon={faKey} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Password"
+                        aria-describedby="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        isInvalid={!!errors.password && touched.password}
+                        className="shadow-none inputPassword"
+                        onBlur={handleBlur}
+                      />
+                      <InputGroup.Text id="iconShowPassword" className={!!errors.password && touched.password ? "isInvalid" : "isValid"} onClick={handleShowPassword}>
+                        <FontAwesomeIcon
+                          icon={showPassword ? faEye : faEyeSlash}
+                        />
+                      </InputGroup.Text>
+                      <Form.Control.Feedback type="invalid">
+                        {!!errors.password && touched.password ? errors.password : null}
 
-                    </Form.Control.Feedback>
-                  </InputGroup>
-                </Form.Group>
+                      </Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
 
-                <Form.Group className="mb-3 mx-4">
-                  <Form.Check
-                    name="rememberMe"
-                    label="Remember me!"
-                    onChange={handleChange}
-                    id="rememberMe"
-                    className="shadow-none checkboxRemember"
-                  />
-                </Form.Group>
-                <div className="d-flex justify-content-center">
-                  <Button className="px-5" variant="danger" type="submit" disabled={!(isValid && dirty) ? true : false }>Submit form</Button>
-                </div>
-              </Form>
-            )}
-        </Formik>
-      </Col>
-    </Row>
-    </Container >
+                  <Form.Group className="mb-3 mx-4">
+                    <Form.Check
+                      name="rememberMe"
+                      label="Remember me!"
+                      onChange={handleChange}
+                      id="rememberMe"
+                      className="shadow-none checkboxRemember"
+                    />
+                  </Form.Group>
+                  <div className="d-flex justify-content-center">
+                    <Button className="px-5" variant="primary" type="submit" disabled={!(isValid && dirty) ? true : false}>Login</Button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </Col>
+        </Row>
+      </Container >
+    </div>
   );
 };
 
